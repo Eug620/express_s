a31e80f6d487cf45aa8af7664e1c80cf6d8ed545
yeyuhang <2418998035@qq.com>
2021-01-26 21:34:17 +0800

    change Table Name

--- routes/db.tableName.js
+++ routes/db.tableName.js
@@ -8,10 +8,10 @@
 
 var SQL_TABLE_NAME = {
     // user: 'express_user_db' // 用户表
-    user: 'Table_user', // 用户表
-    interface: 'Table_interface',
-    interface_log: 'Table_interface_log',
-    image: 'Table_image',
+    user: 'Table_User', // 用户表
+    interface: 'Table_Interface',
+    interface_log: 'Table_Interface_Log',
+    image: 'Table_Image',
     city_provinces: 'Table_City_Provinces', // 省级（省份、直辖市、自治区）
     city_cities: 'Table_City_Cities', // 地级（城市）
     city_areas: 'Table_City_Areas', // 县级（区县)
