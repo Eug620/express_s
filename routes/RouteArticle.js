2b327edd8427156edc641c5068a14b8852beeb8f
yeyuhang <yeyuhang@appicplay.com>
2021-03-25 11:38:22 +0800

    update

---
+++ routes/RouteArticle.js
@@ -0,0 +1,23 @@
+/* 
+ * @Author       : Eug
+ * @Date         : 2021-03-25 10:41:07
+ * @LastEditTime : 2021-03-25 10:51:16
+ * @LastEditors  : Eug
+ * @Descripttion : Descripttion
+ * @FilePath     : /express_s/routes/RouteArticle.js
+ */
+var express = require('express')
+var router = express.Router()
+var { SQL_TABLE_NAME } = require('../lib/const')
+// const UUID = require('uuid')
+const { SEARCHALL } = require('../utils')
+
+// 用户列表
+router.get('/getArticleList', function (req, res, next) {
+  SEARCHALL(SQL_TABLE_NAME.article, (results) => {
+    res.json({ code: 200, result: results })
+  })
+})
+
+module.exports = router
+
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 /* 
  * @Author       : Eug
  * @Date         : 2020-11-19 10:45:35
- * @LastEditTime : 2021-02-19 18:29:13
+ * @LastEditTime : 2021-03-25 10:50:28
  * @LastEditors  : Eug
  * @Descripttion : Descripttion
  * @FilePath     : /express_s/routes/index.js
@@ -11,10 +11,12 @@ var RouteUser = require('./RouteUser');
 var RouteInterface = require('./RouteInterface');
 var RouteImage = require('./RouteImage');
 var RouteCity = require('./RouteCity');
+var RouteArticle = require('./RouteArticle');
 
 module.exports = (app) => {
   app.use('/', RouteInterface)
   app.use('/user', RouteUser)
   app.use('/image', RouteImage)
   app.use('/city', RouteCity)
+  app.use('/article', RouteArticle)
 };
