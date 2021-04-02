994b61eead5d456180d3c9f4ac22fcc932133143
yeyuhang <yeyuhang@appicplay.com>
2021-04-02 10:51:09 +0800

    update

--- routes/RouteArticle.js
+++ routes/RouteArticle.js
@@ -1,7 +1,7 @@
 /* 
  * @Author       : Eug
  * @Date         : 2021-03-25 10:41:07
- * @LastEditTime : 2021-03-26 13:34:25
+ * @LastEditTime : 2021-04-02 10:48:27
  * @LastEditors  : Eug
  * @Descripttion : Descripttion
  * @FilePath     : /express_s/routes/RouteArticle.js
@@ -21,13 +21,19 @@ router.get('/getArticleList', function (req, res, next) {
 
 // 新增文章
 router.post('/createArticle', function (req, res, next) {
-  const { article_title, article_describe, article_content, author } = PARSER(req.body)
+  const { article_title, article_describe, article_content, author, user_id } = PARSER(req.body)
   const timer = Date.parse(new Date())
   if ( !article_title || !article_describe || !article_content || !author) {
     res.json({ code: 403, result: { msg: '参数缺失!' } })
   } else {
-    ADD(SQL_TABLE_NAME.article, "article_id, article_title, article_describe, article_content, author, page_views, create_time", `'${UUID.v4()}', '${article_title}', '${article_describe}', '${article_content}', '${author}', ${0}, ${timer}`, (results, fields) => {
-      res.json({ code: 200, result: { msg: 'create article success' } })
+    SEARCH(SQL_TABLE_NAME.user, `user_name = '${author}' and user_id = '${user_id}'`, (detail) => {
+      if (!detail.length) {
+        res.json({ code: 403, result: { msg: '未登录用户不可新增文章!' } })
+      } else {
+        ADD(SQL_TABLE_NAME.article, "article_id, article_title, article_describe, article_content, author, page_views, create_time", `'${UUID.v4()}', '${article_title}', '${article_describe}', '${article_content}', '${author}', ${0}, ${timer}`, (results, fields) => {
+          res.json({ code: 200, result: { msg: 'create article success' } })
+        })
+      }
     })
   }
 })
