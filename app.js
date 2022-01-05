06eaa75bbbbc08e903e6cd592829de2f844e4152
yeyuhang <yeyuhang@appicplay.com>
2022-01-05 12:22:54 +0800

    ????

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* 
  * @Author       : Eug
  * @Date         : 2020-11-23 15:37:56
- * @LastEditTime : 2022-01-05 12:18:34
+ * @LastEditTime : 2022-01-05 12:21:34
  * @LastEditors  : Eug
  * @Descripttion : Descripttion
  * @FilePath     : /express_s/app.js
@@ -11,7 +11,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var cors = require('cors');
+// var cors = require('cors');
 var bodyParser = require('body-parser');
 
 var loger = require('./loger')
@@ -21,23 +21,23 @@ var Event = require('./event');
 var Libs = require('./utils/libs');
 var app = express();
 
-app.use(cors());
-app.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Headers', req.environ['HTTP_ORIGIN']);
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
-
-// // 处理跨域问题
-// app.all('*', function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', req.headers.origin); //需要显示设置来源
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, test, x-token');
-//   res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Credentials', true); //带cookies
+// app.use(cors());
+// app.all('*', function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header('Access-Control-Allow-Headers', "*");
+//   res.header("Access-Control-Allow-Methods", "*");
+//   res.header("Access-Control-Allow-Credentials", true);
 //   next();
 // });
+
+// 处理跨域问题
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin); //需要显示设置来源
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, test, x-token, authorization');
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Credentials', true); //带cookies
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
