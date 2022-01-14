a3dd96e5da32c77a5879bc61e87cd20bb3dae67e
yeyuhang <yeyuhang@appicplay.com>
2022-01-14 17:38:57 +0800

    user_name

--- routes/RouteInformation.js
+++ routes/RouteInformation.js
@@ -1,7 +1,7 @@
 /* 
  * @Author       : Eug
  * @Date         : 2022-01-14 17:11:25
- * @LastEditTime : 2022-01-14 17:23:16
+ * @LastEditTime : 2022-01-14 17:37:42
  * @LastEditors  : Eug
  * @Descripttion : Descripttion
  * @FilePath     : /express_s/routes/RouteInformation.js
@@ -16,13 +16,23 @@ const { SEARCHALL, BUFFER_UTF8 } = require('../utils')
 // 用户列表
 router.get('/all', function (req, res, next) {
   try {
-    SEARCHALL(SQL_TABLE_NAME.information_record, 'create_time DESC', (results) => {
-      const result = results.map(item => {
-        item['message'] = BUFFER_UTF8(item['message'])
-        return item
-      })
-      res.json({ code: 200, result })
-    })
+    SEARCHALL(
+      SQL_TABLE_NAME.user,
+      'create_time DESC',
+      (user_list) => {
+        const userMap = Object.fromEntries(
+          user_list.map(({user_id, user_name}) => [user_id, user_name])
+        )
+        SEARCHALL(SQL_TABLE_NAME.information_record, 'create_time DESC', (results) => {
+          const result = results.map(item => {
+            item['message'] = BUFFER_UTF8(item['message'])
+            item['user_name'] = userMap[item['user_id']]
+            return item
+          })
+          res.json({ code: 200, result })
+        })
+      }
+    )
   } catch (error) {
     res.json({ code: 500, msg: `${error}` })
   }
